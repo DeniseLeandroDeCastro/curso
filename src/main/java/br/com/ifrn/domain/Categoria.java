e783c4eb644567238af6fabf9b38c9009e9860dd
Denise Castro <dmaria.natal@gmail.com>
2020-12-28 22:39:31 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/br/com/ifrn/domain/Categoria.java
+++ src/main/java/br/com/ifrn/domain/Categoria.java
@@ -2,9 +2,17 @@ package br.com.ifrn.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //Definindo geração automática dos Ids
 	private Integer id;
 	private String nome;
 	
