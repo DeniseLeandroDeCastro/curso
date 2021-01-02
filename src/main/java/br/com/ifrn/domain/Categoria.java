1159b92e2f336e5238ead8b045b6a08ed3cd7b7c
Denise Castro <dmaria.natal@gmail.com>
2021-01-01 21:47:41 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/br/com/ifrn/domain/Categoria.java
+++ src/main/java/br/com/ifrn/domain/Categoria.java
@@ -7,9 +7,10 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import br.com.ifrn.domain.Produto;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -20,6 +21,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/br/com/ifrn/services/CategoriaService.java
+++ src/main/java/br/com/ifrn/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.ifrn.domain.Categoria;
 import br.com.ifrn.repositories.CategoriaRepository;
+import br.com.ifrn.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,6 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-			return obj.orElse(null);	
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
 }
